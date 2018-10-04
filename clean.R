404d624a2a198cac489a3bc4800ad10fdf712fef
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 14:36:31 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,21 +1,21 @@
 
 #Step A: Load and Merge datasets
 #1)	Read in the census dataset (using the function created in HW 3)
-population <- row_data
-my_func <- function(population)
+popState <- row_data
+my_func <- function(popState)
 {
-    population <- population[-c(1,53),-c(1,2,3,4)]
-    colnames(population) <- c("stateNames","population","popOver18","percentOver18")
-    return(population)
+    popState <- popState[-c(1,53),-c(1,2,3,4)]
+    colnames(popState) <- c("stateNames","population","popOver18","percentOver18")
+    return(popState)
 }
-population <- my_func(population)
-head(population)
+popState <- my_func(popState)
+head(popState)
 #2)	Copy the USArrests dataset into a local variable (similar to HW 2)
 arrests <- USArrests
 head(arrests)
 #3) Create a merged dataframe -- with the attributes from both dataframes
-row.names(population) <- population$stateNames
-mergedDF <- merge(population, arrests, by=0, all=TRUE)
+row.names(popState) <- popState$stateNames
+mergedDF <- merge(popState, arrests, by=0, all=TRUE)
 head(mergedDF)
 # Drop the Row.names column
 mergedDF2 <- mergedDF[ -c(1) ]
@@ -26,7 +26,7 @@ head(mergedDF2)
 # a different histogram for the murder rate
 library(ggplot)
 myPlot <- ggplot(mergedDF2)
-myPlot <- myPlot + aes(x=)
+myPlot <- myPlot + aes(x=population)
 
 
 
