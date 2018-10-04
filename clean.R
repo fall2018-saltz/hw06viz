b555a340ed62ed9d324a3c65d2c21ddb6255f822
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 12:25:56 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -3,6 +3,8 @@
 #1)	Read in the census dataset (using the function created in HW 3)
 population <- row_data
 head(population)
+my
+
 #2)	Copy the USArrests dataset into a local variable (similar to HW 2)
 arrests <- USArrests
 head(arrests)
