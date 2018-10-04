dd3d8c92a26827cd3ce17c3feb10dfa14a6f28c2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:49:28 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -5,5 +5,8 @@ population <- row_data
 head(population)
 #2)	Copy the USArrests dataset into a local variable (similar to HW 2)
 arrests <- USArrests
+head(arrests)
+#3) Create a merged dataframe -- with the attributes from both dataframes
+merge(population, arrests, by.x="NAME", by.y="")
 
 
