97304165ea793cee7830aaffbad4fff569d58e8a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:48:23 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,3 +1,6 @@
 
-clean_data <- row_data
-head(clean_data)
+#Step A: Load and Merge datasets
+#1)	Read in the census dataset (using the function created in HW 3)
+population <- row_data
+head(population)
+
