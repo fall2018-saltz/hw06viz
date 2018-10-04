8eab4b66a0e3a22f9d4694b5859892effc455a14
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:51:35 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -7,6 +7,6 @@ head(population)
 arrests <- USArrests
 head(arrests)
 #3) Create a merged dataframe -- with the attributes from both dataframes
-merge(population, arrests, by.x="NAME", by.y="")
+merge(population, arrests)
 
 
