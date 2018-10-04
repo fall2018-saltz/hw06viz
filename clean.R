4c5ef76844fee8fcd53a9ad7cc7509fd9bad4614
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 14:51:23 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -13,23 +13,7 @@ head(popState)
 #2)	Copy the USArrests dataset into a local variable (similar to HW 2)
 arrests <- USArrests
 head(arrests)
-#3) Create a merged dataframe -- with the attributes from both dataframes
-row.names(popState) <- popState$stateNames
-mergedDF <- merge(popState, arrests, by=0, all=TRUE)
-head(mergedDF)
-# Drop the Row.names column
-mergedDF2 <- mergedDF[ -c(1) ]
-head(mergedDF2)
 
-# Step B: Explore the Data – Understanding distributions
-# 4) Create a histogram using GGPLOT for the population and 
-# a different histogram for the murder rate
-
-library(ggplot2)
-myPlot <- ggplot(mergedDF2)
-myPlot <- myPlot + aes(x=population)
-myPlot <- myPlot + geom_histogram(binwidth=1)
-myPlot
 
 
 
