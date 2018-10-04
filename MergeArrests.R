0172fb42c946dd05e00ce878b0a00a4525f9ac6d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 14:52:30 -0400

    Saving MergeArrests.R

--- MergeArrests.R
+++ MergeArrests.R
@@ -1,6 +1,7 @@
 
 #3) Create a merged dataframe -- with the attributes from both dataframes
 popState <- popState
+arrests <- arrests
 row.names(popState) <- popState$stateNames
 mergedDF <- merge(popState, arrests, by=0, all=TRUE)
 head(mergedDF)
