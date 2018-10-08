96cc5b884257dceb627c68d31bbb6374e183ba06
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 12:59:57 -0400

    Saving visualizationC.R

--- visualizationC.R
+++ visualizationC.R
@@ -19,9 +19,12 @@ myPlot9 <- ggplot(merged,aes(x=stateNames,y=MurderNumber))
 myPlot9 = myPlot9 + theme(axis.text.x = element_text(angle = 90))
 myPlot9 = myPlot9 + geom_col() + ggtitle("Total Murders")
 myPlot9
-#4)Generate a new bar chart, the same as in the previous step, but also sort the x-axis by the murder rate
-
-
+#4)Generate a new bar chart, the same as in the previous step,
+#but also sort the x-axis by the murder rate
+myPlot10 <- ggplot(merged,aes(x=stateNames,y=MurderNumber))
+myPlot10 = myPlot10 + theme(axis.text.x = element_text(angle = 90))
+myPlot10 = myPlot10 + geom_col() + ggtitle("Total Murders")
+myPlot10
 
 
 
