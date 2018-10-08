d8f72ddb3bab5b1cbaadbc28df7e2bc58c7d7b41
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 12:41:58 -0400

    Saving visualizationC.R

--- visualizationC.R
+++ visualizationC.R
@@ -5,5 +5,5 @@ tail(merged)
 library(ggplot2)
 merged$MurderNumber <- merged$Murder * 1000000
 head(merged)
-myPlot8 <- ggplot(merged,aes(x=stateNames(angle = 90),y=MurderNumber))
+myPlot8 <- ggplot(merged,aes(x=stateNames,y=MurderNumber))
 myPlot8
