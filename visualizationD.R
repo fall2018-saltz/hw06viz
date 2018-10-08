d7df0337dce9e1a59488f60fe519aae8b3efc385
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 13:09:58 -0400

    Saving visualizationD.R

--- visualizationD.R
+++ visualizationD.R
@@ -3,4 +3,4 @@
 merged <- mergedDF2
 tail(merged)
 library(ggplot2)
-
+#Generate a scatter plot – have population on the X axis, the percent over 18 on the y axis, and the size & color represent the murder rate
