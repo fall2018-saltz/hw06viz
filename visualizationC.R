aec52c4520297ee686a633fbf16ec22e32933df3
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 12:49:14 -0400

    Saving visualizationC.R

--- visualizationC.R
+++ visualizationC.R
@@ -5,14 +5,10 @@ tail(merged)
 library(ggplot2)
 
 #Step C: Which State had the Most Murders – bar charts
-6)	Generate a bar chart, with the number of murders per state. Rotate text (on the X axis), so we can see x labels, also add a title named “Total Murders”. 
-7)	 Generate a new bar chart, the same as in the previous step, but also sort the x-axis by the murder rate 
-8)	 Generate a third bar chart, the same as the previous step, but also showing percentOver18 as the color of the bar
 #1)	Calculate the number of murders per state
 merged$MurderNumber <- merged$Murder * 1000000
 head(merged)
-#Generate a bar chart, with the number of murders per state Hint: use the geom_col function
+#2) Generate a bar chart, with the number of murders per state
 myPlot8 <- ggplot(merged,aes(x=stateNames,y=MurderNumber))
-myPlot8 = myPlot8 + theme(axis.text.x = element_text(angle = 90))
 myPlot8 = myPlot8 +geom_col()
 myPlot8
