6dd69aeb1b29c4076deb7ac2d08a089685818e69
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 11:49:56 -0400

    Saving visualizationB.R

--- visualizationB.R
+++ visualizationB.R
@@ -38,6 +38,9 @@ myPlot5 <- myPlot5 + aes(x=Rape)
 myPlot5 <- myPlot5 + geom_histogram(bins = 30)
 myPlot5
 
+#5) Create a boxplot for the population, and a different boxplot for the murder rate.
+
+
 
 
 
