0860c0c5753fe970875078cd0a643692b5bca54f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 11:29:21 -0400

    Saving visualization.R

--- visualization.R
+++ visualization.R
@@ -2,7 +2,7 @@
 # Step B: Explore the Data – Understanding distributions
 # 4) Create a histogram using GGPLOT for the population and 
 # a different histogram for the murder rate
-merged <- cleanData
+merged <- mergedDF2
 head(merged)
 library(ggplot2)
 myPlot <- ggplot(merged)
