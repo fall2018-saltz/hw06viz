b5ddf6e5891a5cc2d9ba7cd23966c23d8b1e5bef
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 12:45:52 -0400

    Saving visualizationC.R

--- visualizationC.R
+++ visualizationC.R
@@ -7,4 +7,5 @@ merged$MurderNumber <- merged$Murder * 1000000
 head(merged)
 myPlot8 <- ggplot(merged,aes(x=stateNames,y=MurderNumber))
 myPlot8 = myPlot8 + theme(axis.text.x = element_text(angle = 90))
+myPlot8 = myPlot8 +geom_col()
 myPlot8
