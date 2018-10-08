965af225124be0b9b090fd1daac69297911967f7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 12:47:52 -0400

    Saving visualizationC.R

--- visualizationC.R
+++ visualizationC.R
@@ -10,7 +10,7 @@ library(ggplot2)
 8)	 Generate a third bar chart, the same as the previous step, but also showing percentOver18 as the color of the bar
 #1)	Calculate the number of murders per state
 merged$MurderNumber <- merged$Murder * 1000000
-head(merged)5)	Generate a bar chart, with the number of murders per state Hint: use the geom_col function
+head(merged)Generate a bar chart, with the number of murders per state Hint: use the geom_col function
 
 myPlot8 <- ggplot(merged,aes(x=stateNames,y=MurderNumber))
 myPlot8 = myPlot8 + theme(axis.text.x = element_text(angle = 90))
